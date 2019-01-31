9b1083228188b23123acd9e122a5d77a4fa25552
lolors <1010462948@qq.com>
2019-01-31 11:35:19 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mywidget.h"
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MyWidget w;
     w.show();
+    qDebug()<<"hello";
 
     return a.exec();
 }
